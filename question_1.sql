8d44a77597507069d19b87a7e26d8aa7178d8bd1
Likukak <romanabelohoubkova@gmail.com>
2024-11-01 12:15:55 +0100

    Přidávám dotazovací sql k question_1

--- question_1.sql
+++ question_1.sql
@@ -1,4 +1,4 @@
--- Vytvoření pohledu pro průměrné mzdy v jednotlivých letech a odvětvích
+-- Vytvoření pohledu pro průměrné mzdy v jednotlivých letech a odvětvích.
 CREATE OR REPLACE VIEW avg_industry_payroll_per_year AS
 SELECT 
     year,
@@ -8,7 +8,7 @@ SELECT
 FROM t_romana_belohoubkova_project_SQL_primary_final
 GROUP BY year, industry, industry_code;
 
--- Pohled pro přidání předchozích mezd pomocí LAG
+-- Pohled pro přidání předchozích mezd pomocí LAG.
 CREATE OR REPLACE VIEW industry_payroll_yearly_changes AS
 SELECT 
     year,
@@ -18,7 +18,7 @@ SELECT
     LAG(avg_payroll) OVER(PARTITION BY industry_code ORDER BY year) AS prev_year_payroll
 FROM avg_industry_payroll_per_year;
 
--- Pohled pro výpočet procentuální změny a označení trendu
+-- Pohled pro výpočet procentuální změny a označení trendu.
 CREATE OR REPLACE VIEW payroll_trend_analysis AS
 SELECT 
     year,
@@ -38,8 +38,77 @@ SELECT
     END AS trend
 FROM industry_payroll_yearly_changes AS ipyc;
 
--- Ukázkový dotaz pro zobrazení výsledků
-SELECT *
+-- Zjisti odvětví ve kterých mzdy klesaly
+SELECT * 
+FROM payroll_trend_analysis AS pta
+WHERE trend = 'decreased';
+
+-- Zjisti odvětví ve kterých se mzdy v průběhu let nesnižovaly.
+SELECT 
+	industry, 
+	industry_code 
 FROM payroll_trend_analysis AS pta 
-WHERE industry_code = 'A'
-ORDER BY `year` asc;
+WHERE 
+	trend = 'increased' 
+	OR trend = 'no change' 
+GROUP BY industry, industry_code
+HAVING COUNT(*) = (SELECT COUNT(DISTINCT year) FROM payroll_trend_analysis);
+
+-- Zjisti nejvyšší a nejnižší meziroční průměrný procentuální růst.
+SELECT 
+    industry,
+    industry_code,
+    ROUND(AVG(percent_change), 2) AS avg_growth
+FROM payroll_trend_analysis AS pta
+WHERE 
+	year BETWEEN 2006 AND 2018 AND 
+	percent_change NOT IN ('start_year', 'initial_payroll', 'no change')
+GROUP BY industry, industry_code
+ORDER BY avg_growth DESC 
+LIMIT 1;
+
+-- Zjisti odvětví s nějvětší % změnami ve sledovaním období
+SELECT 
+    industry,
+    industry_code,
+    ROUND(SUM(percent_change), 2) AS total_percent_change
+FROM payroll_trend_analysis AS pta
+WHERE 
+	year BETWEEN 2006 AND 2018 AND
+    percent_change NOT IN ('start_year', 'initial_payroll', 'no change')  -- vyloučení textových hodnot
+GROUP BY industry, industry_code
+ORDER BY total_percent_change DESC;
+
+-- Celonárodní průměrná mzda.
+SELECT 
+	year, 
+	ROUND(AVG(avg_payroll), 1) AS national_avg_payroll
+FROM avg_industry_payroll_per_year
+GROUP BY year;
+
+-- Zjisti meziroční procentuální změnu.
+SELECT *
+FROM payroll_trend_analysis AS pta
+WHERE percent_change > 5 OR percent_change < -5
+ORDER BY `year`;
+
+-- Zjisti mzdové trendy za určitý rok.
+SELECT * 
+FROM payroll_trend_analysis AS pta
+WHERE year = 2008;
+
+-- Zjisti v kolik letech mzdy rostly a v kterých odvětvých.
+SELECT 
+	industry, 
+	industry_code, 
+	COUNT(*) AS growth_years
+FROM payroll_trend_analysis AS pta
+WHERE trend = 'increased'
+GROUP BY industry, industry_code
+ORDER BY growth_years DESC;
+
+
+
+
+
+
