482b7f7f9c19d7849b15bcf994ba92a910bb59f2
Likukak <romanabelohoubkova@gmail.com>
2024-11-06 12:13:44 +0100

    úprava SQL dotazů pro question_1

--- question_1.sql
+++ question_1.sql
@@ -27,77 +27,38 @@ SELECT
     avg_payroll,
     prev_year_payroll,
     CASE 
-        WHEN prev_year_payroll IS NULL THEN 'start_year'
+        WHEN prev_year_payroll IS NULL THEN NULL
         ELSE ROUND(((avg_payroll - prev_year_payroll) / prev_year_payroll) * 100, 1)
     END AS percent_change,
     CASE 
-        WHEN prev_year_payroll IS NULL THEN 'initial_payroll'
+        WHEN prev_year_payroll IS NULL THEN NULL
         WHEN avg_payroll > prev_year_payroll THEN 'increased'
         WHEN avg_payroll < prev_year_payroll THEN 'decreased'
         ELSE 'no change'
     END AS trend
 FROM industry_payroll_yearly_changes AS ipyc;
 
--- Zjisti odvětví ve kterých mzdy klesaly
-SELECT * 
+-- Zjisti odvětví ve kterých mzdy klesaly a v kterém roce.
+SELECT DISTINCT 
+    			year, 
+    			industry, 
+    			industry_code, 
+    			avg_payroll, 
+   				percent_change 
 FROM payroll_trend_analysis AS pta
-WHERE trend = 'decreased';
+WHERE trend = 'decreased'
+ORDER BY year, industry;
 
 -- Zjisti odvětví ve kterých se mzdy v průběhu let nesnižovaly.
-SELECT 
-	industry, 
-	industry_code 
-FROM payroll_trend_analysis AS pta 
-WHERE 
-	trend = 'increased' 
-	OR trend = 'no change' 
+SELECT industry, industry_code
+FROM payroll_trend_analysis AS pta
 GROUP BY industry, industry_code
-HAVING COUNT(*) = (SELECT COUNT(DISTINCT year) FROM payroll_trend_analysis);
+HAVING SUM(CASE 
+			WHEN trend = 'decreased' THEN 1 
+			ELSE 0 
+			END) = 0;	
 
--- Zjisti nejvyšší a nejnižší meziroční průměrný procentuální růst.
-SELECT 
-    industry,
-    industry_code,
-    ROUND(AVG(percent_change), 2) AS avg_growth
-FROM payroll_trend_analysis AS pta
-WHERE 
-	year BETWEEN 2006 AND 2018 AND 
-	percent_change NOT IN ('start_year', 'initial_payroll', 'no change')
-GROUP BY industry, industry_code
-ORDER BY avg_growth DESC 
-LIMIT 1;
-
--- Zjisti odvětví s nějvětší % změnami ve sledovaním období
-SELECT 
-    industry,
-    industry_code,
-    ROUND(SUM(percent_change), 2) AS total_percent_change
-FROM payroll_trend_analysis AS pta
-WHERE 
-	year BETWEEN 2006 AND 2018 AND
-    percent_change NOT IN ('start_year', 'initial_payroll', 'no change')  -- vyloučení textových hodnot
-GROUP BY industry, industry_code
-ORDER BY total_percent_change DESC;
-
--- Celonárodní průměrná mzda.
-SELECT 
-	year, 
-	ROUND(AVG(avg_payroll), 1) AS national_avg_payroll
-FROM avg_industry_payroll_per_year
-GROUP BY year;
-
--- Zjisti meziroční procentuální změnu.
-SELECT *
-FROM payroll_trend_analysis AS pta
-WHERE percent_change > 5 OR percent_change < -5
-ORDER BY `year`;
-
--- Zjisti mzdové trendy za určitý rok.
-SELECT * 
-FROM payroll_trend_analysis AS pta
-WHERE year = 2008;
-
--- Zjisti v kolik letech mzdy rostly a v kterých odvětvých.
+-- Zjisti v kolika letech mzdy rostly a v kterých odvětvých.
 SELECT 
 	industry, 
 	industry_code, 
@@ -106,6 +67,62 @@ FROM payroll_trend_analysis AS pta
 WHERE trend = 'increased'
 GROUP BY industry, industry_code
 ORDER BY growth_years DESC;
+
+-- Zjisti v kolika letech mzdy klesaly a v kterých odvětvích.
+SELECT 
+	industry, 
+	industry_code, 
+	COUNT(*) AS growth_years
+FROM payroll_trend_analysis AS pta
+WHERE trend = 'decreased'
+GROUP BY industry, industry_code
+ORDER BY growth_years DESC;
+
+-- Zjisti průměrný meziroční procentuální růst mezd pro každé odvětví.
+SELECT 
+    industry,
+    industry_code,
+    ROUND(AVG(percent_change), 2) AS avg_growth
+FROM payroll_trend_analysis AS pta
+WHERE trend IS NOT NULL 
+  	AND percent_change IS NOT NULL 
+GROUP BY industry, industry_code
+ORDER BY avg_growth DESC;
+
+-- Zjisti největší meziroční procentuální změnu.
+SELECT
+	`year`, 
+	industry_code,
+	industry,
+	MAX(percent_change) AS max_percent_change
+FROM payroll_trend_analysis AS pta
+WHERE percent_change IS NOT NULL
+GROUP BY `year`, industry_code, industry 
+ORDER BY max_percent_change DESC
+LIMIT 1;
+
+-- Zjisti nejzápornější meziroční procentuální změnu.
+SELECT
+	`year`, 
+	industry_code,
+	industry,
+	MIN(percent_change) AS min_percent_change
+FROM payroll_trend_analysis AS pta
+WHERE percent_change IS NOT NULL
+GROUP BY `year`, industry_code, industry 
+ORDER BY min_percent_change ASC 
+LIMIT 1;
+
+-- Zjisti procentuální změny mezi první a poslední výplatou v celém sledovaném období. (Kde si za léta nejvíce polepšili)
+SELECT 
+    industry,
+    industry_code,
+    ROUND(((MAX(avg_payroll) - MIN(avg_payroll)) / MIN(avg_payroll)) * 100, 2) AS total_percent_change
+FROM avg_industry_payroll_per_year AS aippy 
+GROUP BY industry, industry_code
+ORDER BY total_percent_change DESC;
+
+
 
 
 
